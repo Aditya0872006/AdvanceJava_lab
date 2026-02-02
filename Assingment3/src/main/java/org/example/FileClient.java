825bd5d7657dd602370b3c964010b3da8b4f0abe
Aditya Sharma <sharmaadityamj@gmail.com>
2026-02-02 10:39:15 +0530

    java ass-3

---
+++ Assingment3/src/main/java/org/example/FileClient.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.io.*;
+import java.net.*;
+
+public class FileClient {
+    public static void main(String[] args) {
+        try {
+            // Connect to server
+            Socket socket = new Socket("SERVER_IP", 5000);
+            System.out.println("Connected to server");
+
+            // Input stream from server
+            DataInputStream dis =
+                    new DataInputStream(socket.getInputStream());
+
+            // File to save
+            FileOutputStream fos =
+                    new FileOutputStream("received.txt");
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            // Receive file
+            while ((bytesRead = dis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+
+            System.out.println("File received successfully");
+
+            fos.close();
+            dis.close();
+            socket.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
